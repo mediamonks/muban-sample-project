c8bd46c0a161f43516df1be70173c645e1d3d1b7
Arjan van Wijk <thanarie@gmail.com>
2018-02-14 23:02:07 +0100

    Copy static assets on storybook dev

--- build-tools/config/storybook/webpack.config.base.js
+++ build-tools/config/storybook/webpack.config.base.js
@@ -3,6 +3,8 @@
  */
 const path = require('path');
 const merge = require('webpack-merge');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 const config = require('../index');
 
 const {
@@ -63,5 +65,14 @@ module.exports = merge(require('../webpack/webpack.config.base'), {
   },
   plugins: [
     getStyleLintPlugin(config.storybook.enableStyleLintPlugin),
+    new CopyWebpackPlugin([
+      {
+        // copy files to public root (not versioned)
+        context: config.dist.staticPath,
+        from: '**/*',
+        to: config.storybook.buildPath,
+      },
+    ]),
+
   ].filter(_ => _),
 });
